7cec3e84e5fa7ae9f4a3618258352da08e5ffc11
Dai Ngoc <tangocdai13@gmail.com>
2023-07-21 14:05:07 +0700

    run pint

--- laravel-project01/app/Http/Controllers/StudentController.php
+++ laravel-project01/app/Http/Controllers/StudentController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-
 class StudentController extends Controller
 {
     public function index()
@@ -19,7 +16,7 @@ class StudentController extends Controller
         $students = $this->dataStudent();
         $student = null;
         foreach ($students as $item) {
-            if($item['id'] == $id ) {
+            if ($item['id'] == $id) {
                 $student = $item;
                 break;
             }
@@ -29,7 +26,7 @@ class StudentController extends Controller
         }
 
         return view('students.show', [
-            'student' => $student
+            'student' => $student,
         ]);
     }
 
@@ -38,15 +35,15 @@ class StudentController extends Controller
         $students = [
             [
                 'id' => 1,
-                'name' => 'Student 01'
+                'name' => 'Student 01',
             ],
             [
                 'id' => 2,
-                'name' => 'Student 02'
+                'name' => 'Student 02',
             ],
             [
                 'id' => 3,
-                'name' => 'Student 03'
+                'name' => 'Student 03',
             ],
         ];
 
--- laravel-project01/routes/web.php
+++ laravel-project01/routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\OrderController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\Admin\DashboardController;
-
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,14 +21,12 @@ use App\Http\Controllers\Admin\DashboardController;
 */
 
 //dashboard used group prefix
-Route::prefix('admin')->as('admin.')->group(function() {
-   Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-   Route::resource('category', CategoryController::class);
-   Route::resource('customer', CustomerController::class);
-   Route::resource('order', OrderController::class);
-   Route::resource('product', ProductController::class);
-   Route::resource('setting', SettingController::class);
-   Route::resource('user', UserController::class);
+Route::prefix('admin')->as('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('category', CategoryController::class);
+    Route::resource('customer', CustomerController::class);
+    Route::resource('order', OrderController::class);
+    Route::resource('product', ProductController::class);
+    Route::resource('setting', SettingController::class);
+    Route::resource('user', UserController::class);
 });
-
-
