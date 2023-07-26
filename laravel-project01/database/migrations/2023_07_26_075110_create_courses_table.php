b5ad2b5ff8775f74eae14163c11fd5235bada097
Dai Ngoc <tangocdai13@gmail.com>
2023-07-26 16:15:13 +0700

    refactor code for create course & sections table

--- laravel-project01/database/migrations/2023_07_26_075110_create_courses_table.php
+++ laravel-project01/database/migrations/2023_07_26_075110_create_courses_table.php
@@ -13,27 +13,29 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('courses', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name', 255);
-            $table->string('slug', 255)->unique();
-            $table->string('link', 255);
-            $table->decimal('price', $precision = 17, $scale = 2);
-            $table->bigInteger('created_by')->length(20)->comment('Người tạo khoá học');
-            $table->bigInteger('category_id')->length(20)->foreign('category_id')->references('id')->on('categories');
-            $table->integer('lessions')->length(11)->comment('Tổng số bài học trong khoá học');
-            $table->integer('view_count')->length(11)->comment('Tổng số lượt xem');
-            $table->json('benefits');
-            $table->json('fqa');
-            $table->tinyInteger('is_feature')->length(1);
-            $table->tinyInteger('is_online')->length(1);
-            $table->text('description');
-            $table->longText('content');
-            $table->string('meta_title', 255);
-            $table->string('meta_desc', 255);
-            $table->string('meta_keyword', 255);
-            $table->timestamps();
-        });
+        if(!Schema::hasTable('courses')){
+            Schema::create('courses', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name', 255);
+                $table->string('slug', 255)->unique();
+                $table->string('link', 255);
+                $table->decimal('price', $precision = 17, $scale = 2);
+                $table->bigInteger('created_by')->length(20)->comment('Người tạo khoá học');
+                $table->bigInteger('category_id')->length(20)->foreign('category_id')->references('id')->on('categories');
+                $table->integer('lessions')->length(11)->comment('Tổng số bài học trong khoá học');
+                $table->integer('view_count')->length(11)->comment('Tổng số lượt xem');
+                $table->json('benefits');
+                $table->json('fqa');
+                $table->tinyInteger('is_feature')->length(1);
+                $table->tinyInteger('is_online')->length(1);
+                $table->text('description');
+                $table->longText('content');
+                $table->string('meta_title', 255);
+                $table->string('meta_desc', 255);
+                $table->string('meta_keyword', 255);
+                $table->timestamps();
+            });
+        }
     }
 
     /**
--- laravel-project01/database/migrations/2023_07_26_083923_create_sections_table.php
+++ laravel-project01/database/migrations/2023_07_26_083923_create_sections_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        if(!Schema::hasTable('users')){
+        if(!Schema::hasTable('sections')){
             Schema::create('sections', function (Blueprint $table) {
                 $table->increments('id');
                 $table->string('name', 255);
