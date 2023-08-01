fa0237fced76abc4739b43ac2af7fe81a71b36d9
Dai Ngoc <tangocdai13@gmail.com>
2023-08-01 15:59:19 +0700

    Build/Complete build basic Authentication in Laravel

--- laravel-project01/resources/views/welcome.blade.php
+++ laravel-project01/resources/views/welcome.blade.php
@@ -1,12 +1,1 @@
-<h3>Default Route in Laravel</h3>
-
-<a href="{{ route('product') }}">Product</a>
-<a href="{{ route('categories.index') }}">Categories</a>
-<a href="{{ route('news.index') }}">News</a>
-<a href="{{ route('map.index') }}">Maps</a>
-
-
-<li><a href="{{ route('product.show', ['id' => 1]) }}">Product 1</a></li>
-<li><a href="{{ route('product.show', ['id' => 2]) }}">Product 2</a></li>
-<li><a href="{{ route('product.show', ['id' => 3]) }}">Product 3</a></li>
-
+<h2 style="text-align: center">This is my dashboard</h2>
