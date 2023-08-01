8c3970b9600a5f164e0564eb1a58bc66ee55d50a
Dai Ngoc <tangocdai13@gmail.com>
2023-08-01 12:37:18 +0700

    Bug/Authentication

--- laravel-project01/routes/web.php
+++ laravel-project01/routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\CustomerController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\SettingController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,20 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
-//dashboard used group prefix
-Route::prefix('admin')->as('admin.')->group(function () {
-    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('category', CategoryController::class);
-    Route::resource('customer', CustomerController::class);
-    Route::resource('order', OrderController::class);
-    Route::resource('product', ProductController::class);
-    Route::resource('setting', SettingController::class);
-    Route::resource('user', UserController::class);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::resource('employee', EmployeeController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
